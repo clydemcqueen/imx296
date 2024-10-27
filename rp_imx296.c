7bc6e4cdb843616588e487f21746fd14dd872dbd
Clyde McQueen <clyde@mcqueen.net>
2024-10-27 08:57:44 -0700

    Add imx296_get_format

--- rp_imx296.c
+++ rp_imx296.c
@@ -737,6 +737,31 @@ static int imx296_enum_frame_size(struct v4l2_subdev *sd,
 	return 0;
 }
 
+static int imx296_get_format(struct v4l2_subdev *sd,
+			     struct v4l2_subdev_state *state,
+			     struct v4l2_subdev_format *fmt)
+{
+	struct imx296 *sensor = to_imx296(sd);
+
+	if (fmt->which == V4L2_SUBDEV_FORMAT_TRY) {
+		struct v4l2_mbus_framefmt *try_fmt =
+			v4l2_subdev_get_try_format(&sensor->subdev, state, fmt->pad);
+		try_fmt->code = imx296_mbus_code(sensor);
+		fmt->format = *try_fmt;
+	} else {
+		fmt->format.width = sensor->fmt.width;
+		fmt->format.height = sensor->fmt.height;
+		fmt->format.code = sensor->fmt.code;
+		fmt->format.field = sensor->fmt.field;
+		fmt->format.colorspace = sensor->fmt.colorspace;
+		fmt->format.ycbcr_enc = sensor->fmt.ycbcr_enc;
+		fmt->format.quantization = sensor->fmt.quantization;
+		fmt->format.xfer_func = sensor->fmt.xfer_func;
+	}
+
+	return 0;
+}
+
 static int imx296_set_format(struct v4l2_subdev *sd,
 			     struct v4l2_subdev_state *state,
 			     struct v4l2_subdev_format *fmt)
@@ -874,8 +899,8 @@ static const struct v4l2_subdev_video_ops imx296_subdev_video_ops = {
 static const struct v4l2_subdev_pad_ops imx296_subdev_pad_ops = {
 	.enum_mbus_code = imx296_enum_mbus_code,
 	.enum_frame_size = imx296_enum_frame_size,
-	.get_fmt = v4l2_subdev_get_fmt, // TODO
-	.set_fmt = imx296_set_format, // TODO
+	.get_fmt = imx296_get_format,
+	.set_fmt = imx296_set_format,
 	.get_selection = imx296_get_selection,
 	.set_selection = imx296_set_selection,
 	.init_cfg = imx296_init_cfg,
